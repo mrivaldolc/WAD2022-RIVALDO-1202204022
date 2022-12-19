50bdc7c9c0d6f6a28709c8e1930e08ee1189457f
LAPTOP-J22K61CA\Muhammad Rivaldo <muhammadrivaldo276@gmail.com>
2022-12-19 20:30:27 +0700

    fix

---
+++ MODUL4-RIVALDO/login/update.php
@@ -0,0 +1,27 @@
+<?php 
+ 
+include '../config/koneksi.php';
+
+$conn = mysqli_connect("localhost", "root", "", "wad_modul4_rivaldo");
+$profile = mysqli_query($conn, "SELECT * FROM user_rivaldo");
+
+$row = mysqli_fetch_array($profile);
+
+$id = $row['id'];
+$nama = $_POST['nama'];
+$no_hp = $_POST['no_hp'];
+$password = $_POST['password'];
+ 
+
+$update = mysqli_query($conn, "UPDATE user_rivaldo SET nama='$nama', no_hp='$nomor', password='$password' WHERE id='$id'");
+
+
+
+if ($conn==1) {
+    $_SESSION['status']="sudah_update";
+    header("location:../index.php");
+    
+  }
+
+
+?>
--- fol/config/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$koneksi = new mysqli("localhost", "root", "", "rizzomotor");
-
-if (!$koneksi) {
-  die("Koneksi Gagal: " . $koneksi->connect_error);
-}
